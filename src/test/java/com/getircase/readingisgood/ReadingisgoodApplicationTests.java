3289dd092e81735420c58d570de63527c7355d0c
Remziye Sahin <rsahin@cyangate.com>
2022-03-25 10:58:42 +0300

    Created Spring Boot Application

---
+++ src/test/java/com/getircase/readingisgood/ReadingisgoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getircase.readingisgood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingisgoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
